26e39bc7ca866092bb738e5674ef748277590948
JojoDuke <jojoamankwa@gmail.com>
2022-08-05 02:31:29 +0000

    prob

--- netsh/src/customComponents/Footer.jsx
+++ netsh/src/customComponents/Footer.jsx
@@ -1,11 +1,18 @@
-import React,{ useState, useEffect } from 'react'
-import { Nav } from './StyledElements';
+import React,{ useState, useEffect } from 'react';
+import { Nav, NavLink } from './StyledElements';
+import netshLogo from '../assets/netshLogo.png';
+
 
 
 const Footer = () => {
   return (
     <>
-
+      <div style={{ backgroundColor: "black", display: "flex", justifyContent: "space-around", alignItems: "center", width: "100vw", padding: "20px", color: "white", fontFamily: 'Nunito Sans',}}>
+        <img
+          src={netshLogo}
+          width='120px'/>
+        <p>Copyright © 2022 Netsh, <a style={{ color: "white", }} href='https://twitter.com/itsjojoduke1'>Jojo Duke</a></p>
+      </div>
     </>
   )
 }
